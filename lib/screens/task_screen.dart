df487fd7b048349f269928ac593a8b2e8b19c667
pedrolaraburu <pedrolaraburu@gmail.com>
2023-11-26 17:30:56 -0300

    refactor: remove unecessary widgets and fix tasks overflowing the screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter2/provider/task_provider.dart';
 import 'package:provider/provider.dart';
+
 class TaskScreen extends StatefulWidget {
   const TaskScreen({super.key});
 
@@ -9,69 +10,50 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Screen'),
+        title: const Text('Tasks'),
         leading: const Icon(Icons.menu),
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(top: 35, bottom: 20),
-              child: const Text('Tasks'),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SingleChildScrollView(
-                    child: Consumer<TaskProvider>(
-                      builder: (context, TaskProvider taskProvider, child) {
-                        return ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: taskProvider.tasks.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(taskProvider.tasks[index].name),
-                              trailing: 
-                              Wrap(spacing: 12,
-                              children: <Widget>[
-                                Checkbox(
-                                  value: taskProvider.tasks[index].concluido,
-                                  onChanged: (value) {
-                                    Provider.of<TaskProvider>(context, listen: false).toggleTask(taskProvider.tasks[index]);
-                                },
-                              ),
-                                IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () => Provider.of<TaskProvider>(context, listen: false).removeTask(taskProvider.tasks[index])
-                              ),
-                              ],
-                              ) 
-                            );
-                          },
-                        );
+      body: Consumer<TaskProvider>(
+        builder: (context, TaskProvider taskProvider, child) {
+          return ListView.builder(
+            itemCount: taskProvider.tasks.length,
+            itemBuilder: (context, index) {
+              final task = taskProvider.tasks[index];
+              return ListTile(
+                title: Text(task.name),
+                trailing: Wrap(
+                  spacing: 12,
+                  children: <Widget>[
+                    Checkbox(
+                      key: ValueKey('checkbox-${task.id}'),
+                      value: task.concluido,
+                      onChanged: (value) {
+                        Provider.of<TaskProvider>(context, listen: false).toggleTask(task);
                       },
                     ),
-                  ),
-                ],
-              ),
-              )
-          ],
-        ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/form');
-          },
-          backgroundColor: Colors.blue,
-          child: const Icon(Icons.add),
-        )
-      );
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () => Provider.of<TaskProvider>(context, listen: false).removeTask(task),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/form');
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
