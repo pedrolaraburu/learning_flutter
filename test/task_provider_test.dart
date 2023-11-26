0de2f298be28bab53ea632e885ae13f2e8ed435d
pedrolaraburu <pedrolaraburu@gmail.com>
2023-11-26 20:13:14 -0300

    chore: rename test description to english

--- test/task_provider_test.dart
+++ test/task_provider_test.dart
@@ -1,34 +1,32 @@
-// Can you create the TaskProvider test class?
-
 import 'package:learning_flutter2/models/task.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:learning_flutter2/provider/task_provider.dart';
 
 void main() {
   group('TaskProvider', () {
-    test('testInitialLength', () {
+    test('Check if TaskProvider initializes empty', () {
       final taskProvider = TaskProvider(tasks: []);
       expect(taskProvider.tasks.length, 0);
     });
 
-    test('addTask', () {
+    test('Add a task to the list', () {
       final taskProvider = TaskProvider(tasks: []);
-      final task = Task(name: 'test', id: '123', concluido: false);
+      final task = Task(name: 'test', concluido: false);
       taskProvider.addTask(task);
       expect(taskProvider.tasks.length, 1);
     });
 
-    test('removeTask', () {
+    test('Remove a task from the list', () {
       final taskProvider = TaskProvider(tasks: []);
-      final task = Task(name: 'test', id: '123', concluido: false);
+      final task = Task(name: 'test', concluido: false);
       taskProvider.addTask(task);
       taskProvider.removeTask(task);
       expect(taskProvider.tasks.length, 0);
     });
 
-    test('toggleIsDone', () {
+    test('After adding a task, check the checkbox value', () {
       final taskProvider = TaskProvider(tasks: []);
-      final task = Task(name: 'test', id: '123', concluido: false);
+      final task = Task(name: 'test', concluido: false);
       taskProvider.addTask(task);
       taskProvider.toggleTask(task);
       expect(taskProvider.tasks[0].concluido, true);
